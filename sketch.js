5723fa7d8df2c4fb956747033eff82179d271012
ArshiaNarang <70150424+ArshiaNarang@users.noreply.github.com>
2021-01-08 20:51:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var database,form,user;
+var Count=0
+
+function setup() {
+	createCanvas(1000, 1100);
+	database=firebase.database()
+	form=new Form()
+	form.display()
+	user=new userData()
+	user.getCount()
+
+}
+
+function draw() {
+    background("white");
+  
+ 
+ 
+  drawSprites();
+ 
+}
+
+
+	
+
+
+
+
+
